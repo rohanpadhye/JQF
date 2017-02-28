639be62f2de738f5e7122c09a634a680b9f96aa2
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2017-02-27 20:42:47 -0800

    Adding test case with nested classes

---
+++ test/benchmarks/Outer.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2017, University of California, Berkeley
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package benchmarks;
+
+/**
+ * @author Rohan Padhye
+ */
+@SuppressWarnings("all")
+public class Outer {
+    private final int data;
+
+    public Outer(int x) {
+        this.data = x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StaticNested.getInnerInstance(42).getData());
+    }
+
+    static class StaticNested {
+        static Inner getInnerInstance(int x) {
+            return new Outer(x).getInner();
+        }
+    }
+
+    private class Inner {
+        public int getData() {
+            return data;
+        }
+    }
+
+    Inner getInner() {
+        return new Inner();
+    }
+}
