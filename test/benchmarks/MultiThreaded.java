593e01327a8db95621094ea7b6613e5adc761a14
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2017-03-20 13:21:36 -0700

    Minor change to test file

--- test/benchmarks/MultiThreaded.java
+++ test/benchmarks/MultiThreaded.java
@@ -1,25 +1,39 @@
 package benchmarks;
 
+import java.util.TreeSet;
+
 /**
  * @author Rohan Padhye
  */
 public class MultiThreaded {
     public static void main(String[] args) throws InterruptedException {
-        NamePrinter n1 = new NamePrinter("Numba won");
-        NamePrinter n2 = new NamePrinter("Numba too");
+        NamePrinter n0 = new NamePrinter();
+        NamePrinter n1 = new NamePrinter("Numba1");
+        NamePrinter n2 = new NamePrinter("Numba2");
+        n0.start();
         n1.start();
         n2.start();
         n1.join();
         n2.join();
+        n0.join();
     }
 
     static class NamePrinter extends Thread {
+        NamePrinter() {
+            super();
+        }
         NamePrinter(String name) {
             super(name);
         }
         @Override
         public void run() {
+            TreeSet<Character> map = new TreeSet<>();
+            for (char c : this.getName().toCharArray()) {
+                map.add(c);
+            }
+
             System.out.println("I am thread: " + Thread.currentThread().getName());
+            System.out.println(map.contains('u'));
         }
     };
 }
