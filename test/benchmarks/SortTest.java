1542a7345208093003526686d92d2f0bce26184e
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2017-09-13 17:52:24 -0700

    Add sorting benchmark

---
+++ test/benchmarks/SortTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2017, University of California, Berkeley
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package benchmarks;
+
+import java.util.Arrays;
+
+import com.pholser.junit.quickcheck.Mode;
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.Size;
+import com.pholser.junit.quickcheck.guided.GuidedJunitQuickcheckTest;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+
+/**
+ * @author Rohan Padhye
+ */
+public class SortTest extends GuidedJunitQuickcheckTest {
+
+    @BeforeClass
+    public static void ensureTimSortEnabled() {
+        Assert.assertFalse(Boolean.getBoolean(System.getProperty("java.util.Arrays.useLegacyMergeSort")));
+    }
+
+    @Property(mode=Mode.GUIDED, shrink=false, trials=1)
+    public void timSort(Integer @Size(min=1000, max=1000)[] items) {
+        // Sort using TimSort
+        Arrays.sort(items);
+
+        // Assert sorted
+        for (int i = 1; i < items.length; i++) {
+            Assert.assertTrue(items[i-1] <= items[i]);
+        }
+    }
+
+
+    @Property(mode=Mode.GUIDED, shrink=false, trials=1)
+    public void dualPivotQuicksort(int @Size(min=1000, max=1000)[] items) {
+        // Sort using DualPivotQuicksort
+        Arrays.sort(items);
+
+        // Assert sorted
+        for (int i = 1; i < items.length; i++) {
+            Assert.assertTrue(items[i-1] <= items[i]);
+        }
+    }
+
+
+}
