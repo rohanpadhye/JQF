3c9e8b3375579bcbf4fb3b00a6afa957792e8d05
Caroline Lemieux <clemieux@berkeley.edu>
2017-09-18 21:19:59 -0700

    RedBlackBST Example

---
+++ test/benchmarks/RedBlackBSTGenerator.java
@@ -0,0 +1,52 @@
+package benchmarks;
+
+import com.pholser.junit.quickcheck.generator.GenerationStatus;
+import com.pholser.junit.quickcheck.generator.Generator;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.generator.internal.Comparables;
+import com.pholser.junit.quickcheck.random.SourceOfRandomness;
+
+import java.util.function.Predicate;
+
+import static com.pholser.junit.quickcheck.internal.Reflection.defaultValueOf;
+
+/**
+ * Created by clemieux on 9/18/17.
+ */
+public class RedBlackBSTGenerator extends Generator<RedBlackBST> {
+    private int min = (Integer) defaultValueOf(InRange.class, "minInt");
+    private int max = (Integer) defaultValueOf(InRange.class, "maxInt");
+
+    private static final GenerationStatus.Key<Integer> SUBTREE_SIZE = new GenerationStatus.Key<>("treesize", Integer.class);
+
+    public RedBlackBSTGenerator() {
+        super(RedBlackBST.class);
+    }
+
+    /**
+     * Tells this generator to produce values within a specified minimum and/or
+     * maximum, inclusive, with uniform distribution.
+     *
+     * {@link InRange#min} and {@link InRange#max} take precedence over
+     * {@link InRange#minInt()} and {@link InRange#maxInt()}, if non-empty.
+     *
+     * @param range annotation that gives the range's constraints
+     */
+    public void configure(InRange range) {
+        min = range.min().isEmpty() ? range.minInt() : Integer.parseInt(range.min());
+        max = range.max().isEmpty() ? range.maxInt() : Integer.parseInt(range.max());
+    }
+
+    protected Predicate<Integer> inRange() {
+        return Comparables.inRange(min, max);
+    }
+
+    @Override
+    public RedBlackBST<Integer,Integer> generate(SourceOfRandomness sourceOfRandomness, GenerationStatus generationStatus) {
+        Integer size = sourceOfRandomness.nextInt(min,max);
+       // System.out.println("size: " + size.toString());
+        generationStatus.setValue(SUBTREE_SIZE, size);
+        RedBlackBSTNodeGenerator generator = new RedBlackBSTNodeGenerator();
+        return new RedBlackBST<>(generator.generate(sourceOfRandomness, generationStatus));
+    }
+}
