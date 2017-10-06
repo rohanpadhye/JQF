099857a7b6293a70cedb47b9419433244d94b072
Caroline Lemieux <clemieux@berkeley.edu>
2017-10-05 18:15:16 -0700

    naive sort tests

--- examples/src/test/java/edu/berkeley/cs/jqf/examples/jdk/SortTest.java
+++ examples/src/test/java/edu/berkeley/cs/jqf/examples/jdk/SortTest.java
@@ -49,6 +49,132 @@ public class SortTest {
         Assert.assertFalse(Boolean.getBoolean("java.jdk.Arrays.useLegacyMergeSort"));
     }
 
+
+
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    private void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        int L[] = new int [n1];
+        int R[] = new int [n2];
+
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i)
+            L[i] = arr[l + i];
+        for (int j=0; j<n2; ++j)
+            R[j] = arr[m + 1+ j];
+
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    private void mergeSort(int arr[], int l, int r)
+    {
+        if (l < r)
+        {
+            // Find the middle point
+            int m = (l+r)/2;
+
+            // Sort first and second halves
+            mergeSort(arr, l, m);
+            mergeSort(arr , m+1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+    @Fuzz
+    public void mergeSortTest(int @Size(min=200, max=200)[] items){
+        mergeSort(items, 0, items.length-1);
+    }
+
+    // from http://www.java2novice.com/java-sorting-algorithms/quick-sort/
+    private void quickSort(int[] array, int lowerIndex, int higherIndex) {
+
+        int i = lowerIndex;
+        int j = higherIndex;
+        // calculate pivot number, I am taking pivot as middle index number
+        int pivot = array[lowerIndex+(higherIndex-lowerIndex)/2];
+        // Divide into two arrays
+        while (i <= j) {
+            /**
+             * In each iteration, we will identify a number from left side which
+             * is greater then the pivot value, and also we will identify a number
+             * from right side which is less then the pivot value. Once the search
+             * is done, then we exchange both numbers.
+             */
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                //move index to next position on both sides
+                i++;
+                j--;
+            }
+        }
+        // call quickSort() method recursively
+        if (lowerIndex < j)
+            quickSort(array, lowerIndex, j);
+        if (i < higherIndex)
+            quickSort(array, i, higherIndex);
+    }
+
+    @Fuzz
+    public void quickSortTest(int @Size(min=200, max=200)[] items){
+        quickSort(items, 0, items.length-1);
+    }
+
     @Fuzz
     public void timSort(Integer @Size(min=200, max=200)[] items) {
         // Sort using TimSort
@@ -111,6 +237,11 @@ public class SortTest {
         for (int i = 1; i < items.length; i++) {
             Assert.assertTrue(items[i-1] <= items[i]);
         }
+    }
+
+    @Fuzz
+    public void allSorts(int @Size(min=20, max= 400)[] items){
+        Arrays.sort(items);
     }
 
 
