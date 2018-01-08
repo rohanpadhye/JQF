283bb31ce22f1378c6b22e3f3eef314b5b5c1ecc
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2018-01-08 18:18:17 +0530

    ExecutionIndexing: Support stacked and multi-byte mutations

--- fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/guidance/ExecutionIndexingGuidance.java
+++ fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/guidance/ExecutionIndexingGuidance.java
@@ -32,10 +32,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 import edu.berkeley.cs.jqf.fuzz.util.Coverage;
 import edu.berkeley.cs.jqf.fuzz.util.ExecutionIndex;
@@ -45,7 +47,19 @@ import edu.berkeley.cs.jqf.instrument.tracing.events.CallEvent;
 import edu.berkeley.cs.jqf.instrument.tracing.events.ReturnEvent;
 import edu.berkeley.cs.jqf.instrument.tracing.events.TraceEvent;
 
+import static java.lang.Math.ceil;
+import static java.lang.Math.log;
+
 /**
+ * A guidance that performs coverage-guided fuzz testing, where inputs
+ * are represented not as sequences of bytes but instead as maps of
+ * execution indexes to bytes.
+ *
+ * <p>Whenever the input generator for a test requests a new byte, the
+ * execution index of that event is used to query a value in the input
+ * map. This representation retains much more structure of the input
+ * than a simple linear sequence.</p>
+ *
  * @author Rohan Padhye
  */
 public class ExecutionIndexingGuidance implements Guidance {
@@ -83,6 +97,12 @@ public class ExecutionIndexingGuidance implements Guidance {
 
     /** Whether to use real execution indexes as opposed to flat numbering (for evaluation). */
     private boolean realExecutionIndex = true;
+
+    /** Mean number of mutations to perform in each round. */
+    private static final double MEAN_MUTATION_COUNT = 1.2;
+
+    /** Mean number of contiguous bytes to mutate in each mutation. */
+    private static final double MEAN_MUTATION_SIZE = 1.5; // Bytes
 
     /**
      * Creates a new execution-index-parametric guidance.
@@ -216,19 +236,48 @@ public class ExecutionIndexingGuidance implements Guidance {
     }
 
 
+    /**
+     * A candidate test input represented as a map from execution indices
+     * to integer values.
+     *
+     * <p>When a quickcheck-like generator requests a new ``random'' byte,
+     * the current execution index is used to retrieve the input from
+     * this input map (a fresh value is generated and stored in the map
+     * if the key is not mapped).</p>
+     *
+     * <p>Inputs should not be publicly mutable. The only way to mutate
+     * an input is via the {@link #fuzz} method which produces a new input
+     * object with some values mutated.</p>
+     */
     public static class Input {
 
         private Map<ExecutionIndex, Integer> valuesMap;
         private Collection<ExecutionIndex> requiredKeys = new ArrayList<>();
 
+        /**
+         * Create an empty input map.
+         */
         public Input() {
             valuesMap = new LinkedHashMap<>();
         }
 
+        /**
+         * Create a copy of an existing input map.
+         *
+         * @param toClone the input map to clone
+         */
         public Input(Input toClone) {
             valuesMap = new LinkedHashMap<>(toClone.valuesMap);
         }
 
+        /**
+         * Retrieve a value for an execution index if mapped, else generate
+         * a fresh value.
+         *
+         * @param key    the execution index of the trace event requesting a new byte
+         * @param random the PRNG
+         * @return the value to return to the quickcheck-like generator
+         */
         public int getOrGenerateFresh(ExecutionIndex key, Random random) {
             // Try to get existing values
             Integer val = valuesMap.get(key);
@@ -244,6 +293,14 @@ public class ExecutionIndexingGuidance implements Guidance {
             return val;
         }
 
+        /**
+         * Trims the input map of all keys that were never actually requested since
+         * its construction.
+         *
+         * <p>Although this operation mutates the underlying object, the effect should
+         * not be externally visible (at least as long as the test executions are
+         * deterministic).</p>
+         */
         public void gc() {
             Map<ExecutionIndex, Integer> newMap = new LinkedHashMap<>(valuesMap.size());
             for (ExecutionIndex key : requiredKeys) {
@@ -253,24 +310,63 @@ public class ExecutionIndexingGuidance implements Guidance {
         }
 
 
-        private void mutate(Random random) {
+        /**
+         * Performs a single contiguous mutation on the current input.
+         *
+         * <p>The size of the mutation is randomly sampled from a
+         * geometric distribution with mean
+         * {@link #MEAN_MUTATION_SIZE}.</p>
+         *
+         * @param random the PRNG
+         * @param mutation the unary operation to apply on an integer value
+         */
+        private void mutate(Random random, UnaryOperator<Integer> mutation) {
+            // Select a random offset and size
             int idx = random.nextInt(valuesMap.size());
-            int i = 0;
-            for (Map.Entry<?, Integer> e: valuesMap.entrySet()) {
-                if (i == idx) {
-                    int val = random.nextInt(256);
-                    e.setValue(val);
-                    break;
-                } else {
-                    i++;
+            int mutationSize = sampleGeometric(MEAN_MUTATION_SIZE, random);
+
+            // Iterate over all entries in the value map
+            Iterator<Map.Entry<ExecutionIndex, Integer>> entryIterator
+                    = valuesMap.entrySet().iterator();
+            for (int i = 0; entryIterator.hasNext(); i++) {
+                Map.Entry<ExecutionIndex, Integer> e = entryIterator.next();
+                // Only mutate `mutationSize` contiguous entries from
+                // the randomly selected `idx`.
+                if (i >= idx && i < (idx + mutationSize)) {
+                    // Apply the provided mutation operation
+                    int mutatedValue = mutation.apply(e.getValue());
+                    e.setValue(mutatedValue);
                 }
             }
         }
 
+        /**
+         * Return a new input derived from this one with some values
+         * mutated.
+         *
+         * <p>This method performs multiple stacked mutations on
+         * the input map, where the number of mutations is randomly
+         * sampled from a geometric distribution with mean
+         * {@link #MEAN_MUTATION_COUNT}</p>
+         *
+         * @param random the PRNG
+         * @return a newly fuzzed input
+         */
         public Input fuzz(Random random) {
+            // Derive new input from this object as source
             Input newInput = new Input(this);
-            newInput.mutate(random);
+            // Stack a bunch of mutations
+            int numMutations = sampleGeometric(MEAN_MUTATION_COUNT, random);
+            for (int i = 0; i < numMutations; i++) {
+                newInput.mutate(random, (x) -> random.nextInt(256));
+            }
             return newInput;
+        }
+
+        private int sampleGeometric(double mean, Random random) {
+            double p = 1 / mean;
+            double uniform = random.nextDouble();
+            return (int) ceil(log(1 - uniform) / log(1 - p));
         }
 
     }
