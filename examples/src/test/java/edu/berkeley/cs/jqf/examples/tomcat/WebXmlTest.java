c4f2864586a9f8e864d86aa825d9b874d25d6204
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2018-02-22 12:48:49 -0800

    Examples: Add Apache Commons Lang and Tomcat WebXml

---
+++ examples/src/test/java/edu/berkeley/cs/jqf/examples/tomcat/WebXmlTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2018, University of California, Berkeley
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.berkeley.cs.jqf.examples.tomcat;
+
+import java.io.InputStream;
+
+import com.pholser.junit.quickcheck.From;
+import edu.berkeley.cs.jqf.examples.XmlDocumentGenerator;
+import edu.berkeley.cs.jqf.examples.common.Dictionary;
+import edu.berkeley.cs.jqf.fuzz.junit.Fuzz;
+import edu.berkeley.cs.jqf.fuzz.junit.quickcheck.JQF;
+import org.apache.tomcat.util.descriptor.web.WebXml;
+import org.apache.tomcat.util.descriptor.web.WebXmlParser;
+import org.junit.runner.RunWith;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+@RunWith(JQF.class)
+public class WebXmlTest {
+
+    @Fuzz
+    public void testWithSequence(InputStream in) {
+        InputSource inputSource = new InputSource(in);
+        WebXml webXml = new WebXml();
+        WebXmlParser parser = new WebXmlParser(false, false, true);
+        parser.parseWebXml(inputSource, webXml, false);
+    }
+
+    @Fuzz
+    public void testWithGenerator(@From(XmlDocumentGenerator.class) @Dictionary("dictionaries/tomcat-webxml.dict") Document dom) {
+        testWithSequence(XmlDocumentGenerator.documentToInputStream(dom));
+    }
+
+    @Fuzz
+    public void debug(@From(XmlDocumentGenerator.class) @Dictionary("dictionaries/tomcat-webxml.dict") Document dom) {
+        System.out.println(XmlDocumentGenerator.documentToString(dom));
+        testWithGenerator(dom);
+    }
+}
