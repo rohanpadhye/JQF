32ce565da8e97d871248f0112f32c777379a3e4e
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2019-06-18 14:41:24 -0700

    Examples: Add test for robustness

---
+++ examples/src/test/java/edu/berkeley/cs/jqf/examples/RobustnessTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2019, The Regents of the University of California
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.berkeley.cs.jqf.examples;
+
+import com.pholser.junit.quickcheck.From;
+import edu.berkeley.cs.jqf.examples.common.AsciiStringGenerator;
+import edu.berkeley.cs.jqf.fuzz.Fuzz;
+import edu.berkeley.cs.jqf.fuzz.JQF;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+@RunWith(JQF.class)
+public class RobustnessTest {
+    @Fuzz
+    public void testTwo(@From(AsciiStringGenerator.class) String s1, @From(AsciiStringGenerator.class) String s2) {
+        if (s2.length() == 3) {
+            if (s2.charAt(0) == 'a') {
+                if (s2.charAt(1) == 'b') {
+                    if (s2.charAt(2) == 'c') {
+                        if (s1.length() == 3) {
+                            if (s1.charAt(0) == 'a') {
+                                if (s1.charAt(1) == 'b') {
+                                    if (s1.charAt(2) == 'c') {
+                                        assertTrue(false);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Fuzz
+    public void debugTwo(@From(AsciiStringGenerator.class) String s1, @From(AsciiStringGenerator.class) String s2) {
+        System.out.print(s2.length() + ": ");
+        System.out.println(s2);
+        System.out.print(s1.length() + ": ");
+        System.out.println(s1);
+    }
+
+
+}
