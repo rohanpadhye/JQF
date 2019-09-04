89834ac4e3feccf059cad0f1280a510623c6bd65
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2019-09-04 11:07:39 -0700

    Guidance: Add optional method for observing generated args

--- fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/junit/quickcheck/FuzzStatement.java
+++ fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/junit/quickcheck/FuzzStatement.java
@@ -139,6 +139,9 @@ public class FuzzStatement extends Statement {
                         args = generators.stream()
                                 .map(g -> g.generate(random, genStatus))
                                 .toArray();
+
+                        // Let guidance observe the generated input args
+                        guidance.observeGeneratedArgs(args);
                     } catch (IllegalStateException e) {
                         if (e.getCause() instanceof EOFException) {
                             // This happens when we reach EOF before reading all the random values.
