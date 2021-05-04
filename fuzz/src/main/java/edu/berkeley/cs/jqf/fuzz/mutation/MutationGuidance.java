ec894ab22c259272fbf1a728ed4d551210e664aa
Bella Laybourn <ilaybour@andrew.cmu.edu>
2021-05-03 23:32:46 -0800

    fix-ups

--- fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/mutation/MutationGuidance.java
+++ fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/mutation/MutationGuidance.java
@@ -8,7 +8,6 @@ import edu.berkeley.cs.jqf.fuzz.junit.TrialRunner;
 import edu.berkeley.cs.jqf.instrument.InstrumentationException;
 import edu.berkeley.cs.jqf.instrument.tracing.TraceLogger;
 import edu.berkeley.cs.jqf.instrument.tracing.events.KillEvent;
-import edu.berkeley.cs.jqf.instrument.tracing.events.TraceEvent;
 import mutation.CartographyClassLoader;
 import mutation.MutationInstance;
 import org.junit.AssumptionViolatedException;
@@ -82,40 +81,83 @@ public class MutationGuidance extends ZestGuidance {
 
     @Override
     public void handleResult(Result result, Throwable error) throws GuidanceException {
+        // Stop timeout handling
         this.runStart = null;
+
+        // Increment run count
         this.numTrials++;
+
         boolean valid = result == Result.SUCCESS;
+
         if (valid) {
+            // Increment valid counter
             numValid++;
         }
+
         if (result == Result.SUCCESS || (result == Result.INVALID && SAVE_ONLY_VALID == false)) {
+
             int prevMutants = ((MutationCoverage) totalCoverage).numCaughtMutants();
-            //int prevSeen = ((MutationCoverage) totalCoverage).numSeenMutants();
             boolean mutantsUpdated = ((MutationCoverage) totalCoverage).updateMutants(((MutationCoverage) runCoverage));
             int newMutants = ((MutationCoverage) totalCoverage).numCaughtMutants();
-            //int newSeen = ((MutationCoverage) totalCoverage).numSeenMutants();
-            /*if(newSeen > prevSeen) {
-                System.out.println("saw: " + ((MutationCoverage) totalCoverage).seenMutants);
-            }*/
+            Set<Object> runMutants = ((MutationCoverage) runCoverage).getMutants();
+
+            // Coverage before
+            int nonZeroBefore = totalCoverage.getNonZeroCount();
             int validNonZeroBefore = validCoverage.getNonZeroCount();
+
+            // Compute a list of keys for which this input can assume responsiblity.
+            // Newly covered branches are always included.
+            // Existing branches *may* be included, depending on the heuristics used.
+            // A valid input will steal responsibility from invalid inputs
+            Set<Object> responsibilities = computeResponsibilities(valid);
+
+            // Update total coverage
+            boolean coverageBitsUpdated = totalCoverage.updateBits(runCoverage);
             if (valid) {
                 validCoverage.updateBits(runCoverage);
             }
+
+            // Coverage after
+            int nonZeroAfter = totalCoverage.getNonZeroCount();
+            if (nonZeroAfter > maxCoverage) {
+                maxCoverage = nonZeroAfter;
+            }
             int validNonZeroAfter = validCoverage.getNonZeroCount();
-            Set<Object> runMutants = ((MutationCoverage) runCoverage).getMutants();
-            runCoverage.clear();
+
+            // Possibly save input
             boolean toSave = false;
             String why = "";
+
             if (mutantsUpdated) {
                 toSave = true;
                 why = why + "+mutants";
             }
-            if (this.validityFuzzing && validNonZeroAfter > validNonZeroBefore) {
-                currentInput.setValid(true);
+
+            if (!DISABLE_SAVE_NEW_COUNTS && coverageBitsUpdated) {
+                toSave = true;
+                why = why + "+count";
+            }
+
+            // Save if new total coverage found
+            if (nonZeroAfter > nonZeroBefore) {
+                // Must be responsible for some branch
+                assert(responsibilities.size() > 0);
+                toSave = true;
+                why = why + "+cov";
+            }
+
+            // Save if new valid coverage is found
+            /*if (this.validityFuzzing && validNonZeroAfter > validNonZeroBefore) {
+                // Must be responsible for some branch
+                assert(responsibilities.size() > 0);
+                currentInput.valid = true;
                 toSave = true;
                 why = why + "+valid";
-            }
+            }*/
+
             if (toSave) {
+
+                // Trim input (remove unused keys)
                 currentInput.gc();
 
                 // It must still be non-empty
@@ -129,22 +171,19 @@ public class MutationGuidance extends ZestGuidance {
                 infoLog("Saving new input (at run %d): " +
                                 "input #%d " +
                                 "of size %d; " +
-                                "total coverage = %d mutants",
+                                "total coverage = %d",
                         numTrials,
                         savedInputs.size(),
                         currentInput.size(),
-                        newMutants);
+                        nonZeroAfter);
 
                 // Save input to queue and to disk
                 final String reason = why;
-                //TODO give responsibility for mutants caught
-                if(newMutants > prevMutants) {
-                    System.out.println("caught mutant! " + runMutants);
-                }
-                GuidanceException.wrap(() -> saveCurrentInput(runMutants, reason));
+                responsibilities.addAll(runMutants);
+                GuidanceException.wrap(() -> saveCurrentInput(responsibilities, reason));
+
             }
-        }
-        else if (result == Result.FAILURE || result == Result.TIMEOUT) {
+        } else if (result == Result.FAILURE || result == Result.TIMEOUT) {
             String msg = error.getMessage();
 
             // Get the root cause of the failure
@@ -155,6 +194,7 @@ public class MutationGuidance extends ZestGuidance {
 
             // Attempt to add this to the set of unique failures
             if (uniqueFailures.add(Arrays.asList(rootCause.getStackTrace()))) {
+
                 // Trim input (remove unused keys)
                 currentInput.gc();
 
@@ -226,34 +266,49 @@ public class MutationGuidance extends ZestGuidance {
 
     @Override
     public void run(TestClass testClass, FrameworkMethod method, Object[] args) throws Throwable {
+        //System.out.println("running " + testClass.getName());
         // BufferedWriter writer = null; //TODO
         new TrialRunner(testClass.getJavaClass(), method, args).run(); //loaded by CartographyClassLoader
+        //System.out.println("loaded " + testClass.getName());
         long totalRun = 0, totalFail = 0, totalIgnore = 0;
         List<Throwable> fails = new ArrayList<>();
         List<Class<?>> expectedExceptions = Arrays.asList(method.getMethod().getExceptionTypes());
         for(MutationInstance mcl : cartographyClassLoader.getCartograph()) {
-            try {
-                new TrialRunner(Class.forName(testClass.getName(), true, mcl), method, args).run();
-            } catch(InstrumentationException e) {
-                throw new GuidanceException(e);
-            } catch (GuidanceException e) {
-                throw e;
-            } catch (AssumptionViolatedException | TimeoutException e) {
-                totalIgnore++; //TODO track separately? if everything times out...
-                //TODO consider how to get timeout (stop infinite loops) - could use instrumentingclassloader, but that's really slow
-                // could try with thread.stop (works, but deprecated/dangerous)
-                // could move toward running all mutants in parallel with this sort of thing
-            } catch (Throwable e) {
-                if (!isExceptionExpected(e.getClass(), expectedExceptions)) {
-                    totalFail++;
-                    mcl.kill();
-                    TraceLogger.get().emit(new KillEvent(0, null, 0, mcl)); //temp 0 values
-                    fails.add(e);
+            if(!mcl.isDead()) {
+                try {
+                    //System.out.println("testClass: " + testClass.getName() + ",  method: " + method.getName());
+                    //System.out.println("running mutant " + mcl);
+                    Class<?> clazz = Class.forName(testClass.getName(), true, mcl);
+                    //TODO don't want to hardcode args
+                    //TODO make cartographyclassloader extend instrumentingclassloader --> code coverage + mutants
+                    // or just include instructiontransformer
+                    new TrialRunner(clazz, new FrameworkMethod(clazz.getMethod(method.getName(), List.class)), args).run();
+                } catch (InstrumentationException e) {
+                    throw new GuidanceException(e);
+                } catch (GuidanceException e) {
+                    throw e;
+                } catch (AssumptionViolatedException | TimeoutException e) {
+                    totalIgnore++; //TODO track separately? if everything times out...
+                    //TODO consider how to get timeout (stop infinite loops) - could use instrumentingclassloader, but that's really slow
+                    // could try with thread.stop (works, but deprecated/dangerous)
+                    // could move toward running all mutants in parallel with this sort of thing
+                } catch (Throwable e) {
+                    if (!isExceptionExpected(e.getClass(), expectedExceptions)) {
+                        totalFail++;
+                    /*if(!mcl.isDead()) {
+                        System.out.println("exception for mutant " + mcl + ": " + e.getClass());
+                        // e.printStackTrace();
+                    }*/
+                        mcl.kill();
+                        TraceLogger.get().emit(new KillEvent(0, null, 0, mcl)); //temp 0 values
+                        fails.add(e);
+                    }
                 }
+                totalRun++; //TODO mutant not killed - indicate?
+                ((MutationCoverage) runCoverage).see(mcl);
             }
-            totalRun++; //TODO mutant not killed - indicate?
-            ((MutationCoverage) runCoverage).see(mcl);
         }
+        //System.out.println("run " + testClass.getName());
         /*writer.write("Totals:\nFailures: " + totalFail + ", Ignored: " + totalIgnore + ", Run: " + totalRun + "\nFailure List:\n");
         for(Throwable fail : fails) {
             writer.write(fail + "\n");
@@ -320,6 +375,8 @@ public class MutationGuidance extends ZestGuidance {
                 console.printf("Queue size:           %,d (%,d favored last cycle)\n", savedInputs.size(), numFavoredLastCycle);
                 console.printf("Current parent input: %s\n", currentParentInputDesc);
                 console.printf("Execution speed:      %,d/sec now | %,d/sec overall\n", intervalExecsPerSec, execsPerSec);
+                console.printf("Total coverage:       %,d branches (%.2f%% of map)\n", nonZeroCount, nonZeroFraction);
+                console.printf("Valid coverage:       %,d branches (%.2f%% of map)\n", nonZeroValidCount, nonZeroValidFraction);
                 console.printf("Total coverage:       %,d mutants\n", ((MutationCoverage) totalCoverage).numCaughtMutants());
                 console.printf("Available to Cover:   %,d mutants\n",((MutationCoverage) totalCoverage).numSeenMutants());
             }
@@ -333,116 +390,14 @@ public class MutationGuidance extends ZestGuidance {
 
     }
 
+    @Override
+    protected String getTitle() {
+        if (blind) {
+            return  "Generator-based random fuzzing (no guidance)\n" +
+                    "--------------------------------------------\n";
+        } else {
+            return  "Mutation-Guided Fuzzing\n" +
+                    "--------------------------\n";
+        }
+    }
 }
-
-/*@Override
-    public void handleResult(Result result, Throwable error) throws GuidanceException {
-        this.runStart = null;
-        this.numTrials++;
-        boolean valid = result == Result.SUCCESS;
-        if (valid) {
-            numValid++;
-        }
-
-        //TODO assumption failure -> not valid coverage, still total coverage
-        if (result == Result.SUCCESS || (result == Result.INVALID && SAVE_ONLY_VALID == false)) {
-            int prevMutants = ((MutationCoverage) totalCoverage).numCaughtMutants();
-            int validNonZeroBefore = validCoverage.getNonZeroCount();
-            boolean coverageBitsUpdated = totalCoverage.updateBits(runCoverage);
-            boolean mutantsUpdated = ((MutationCoverage) totalCoverage).updateMutants(((MutationCoverage) runCoverage));
-            if (valid) {
-                validCoverage.updateBits(runCoverage);
-            }
-            int validNonZeroAfter = validCoverage.getNonZeroCount();
-            int newMutants = ((MutationCoverage) totalCoverage).numCaughtMutants();
-
-            boolean toSave = false;
-            String why = "";
-            if (mutantsUpdated) {
-                toSave = true;
-                why = why + "+mutants";
-            }
-            if (this.validityFuzzing && validNonZeroAfter > validNonZeroBefore) {
-                currentInput.setValid(true);
-                toSave = true;
-                why = why + "+valid";
-            }
-
-            if (toSave) {
-                currentInput.gc();
-
-                // It must still be non-empty
-                assert(currentInput.size() > 0) : String.format("Empty input: %s", currentInput.desc);
-
-                // libFuzzerCompat stats are only displayed when they hit new coverage
-                if (LIBFUZZER_COMPAT_OUTPUT) {
-                    displayStats();
-                }
-
-                infoLog("Saving new input (at run %d): " +
-                                "input #%d " +
-                                "of size %d; " +
-                                "total coverage = %d mutants",
-                        numTrials,
-                        savedInputs.size(),
-                        currentInput.size(),
-                        newMutants);
-
-                // Save input to queue and to disk
-                final String reason = why;
-                //TODO give responsibility for mutants caught
-                GuidanceException.wrap(() -> saveCurrentInput(((MutationCoverage) totalCoverage).getMutants(), reason));
-            }
-        } else if (result == Result.FAILURE || result == Result.TIMEOUT) {
-            String msg = error.getMessage();
-
-            // Get the root cause of the failure
-            Throwable rootCause = error;
-            while (rootCause.getCause() != null) {
-                rootCause = rootCause.getCause();
-            }
-
-            // Attempt to add this to the set of unique failures
-            if (uniqueFailures.add(Arrays.asList(rootCause.getStackTrace()))) {
-                // Trim input (remove unused keys)
-                currentInput.gc();
-
-                // It must still be non-empty
-                assert(currentInput.size() > 0) : String.format("Empty input: %s", currentInput.desc);
-
-                // Save crash to disk
-                int crashIdx = uniqueFailures.size()-1;
-                String saveFileName = String.format("id_%06d", crashIdx);
-                File saveFile = new File(savedFailuresDirectory, saveFileName);
-                GuidanceException.wrap(() -> writeCurrentInputToFile(saveFile));
-                infoLog("%s","Found crash: " + error.getClass() + " - " + (msg != null ? msg : ""));
-                String how = currentInput.desc;
-                String why = result == Result.FAILURE ? "+crash" : "+hang";
-                infoLog("Saved - %s %s %s", saveFile.getPath(), how, why);
-
-                if (EXACT_CRASH_PATH != null && !EXACT_CRASH_PATH.equals("")) {
-                    File exactCrashFile = new File(EXACT_CRASH_PATH);
-                    GuidanceException.wrap(() -> writeCurrentInputToFile(exactCrashFile));
-                }
-
-                // libFuzzerCompat stats are only displayed when they hit new coverage or crashes
-                if (LIBFUZZER_COMPAT_OUTPUT) {
-                    displayStats();
-                }
-
-            }
-        }
-
-        // displaying stats on every interval is only enabled for AFL-like stats screen
-        if (!LIBFUZZER_COMPAT_OUTPUT) {
-            displayStats();
-        }
-
-        // Save input unconditionally if such a setting is enabled
-        if (LOG_ALL_INPUTS) {
-            File logDirectory = new File(allInputsDirectory, result.toString().toLowerCase());
-            String saveFileName = String.format("id_%09d", numTrials);
-            File saveFile = new File(logDirectory, saveFileName);
-            GuidanceException.wrap(() -> writeCurrentInputToFile(saveFile));
-        }
-    }*/
