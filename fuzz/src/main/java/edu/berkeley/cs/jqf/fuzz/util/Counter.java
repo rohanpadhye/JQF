9fc87f2423b6b36f48401af6c6f26e707d4568cf
Raffi Sanna <rvs314@protonmail.com>
2021-07-09 17:36:53 -0400

    Add toString method to Counter/Coverage

--- fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/util/Counter.java
+++ fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/util/Counter.java
@@ -32,6 +32,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Maps integer keys to integer counts using a fixed-size table.
@@ -229,5 +230,10 @@ public class Counter {
 
     public void setAtIndex(int idx, int value) {
         this.counts[idx] = value;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.stream(counts).mapToObj(Integer::toString).collect(Collectors.joining(","));
     }
 }
