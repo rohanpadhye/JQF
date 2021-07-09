269755ee8783b24d290022020c61e92d8d7788a2
Raffi Sanna <rvs314@protonmail.com>
2021-07-09 17:38:45 -0400

    Use toString instead of hashCode

--- fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/ei/ZestGuidance.java
+++ fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/ei/ZestGuidance.java
@@ -482,7 +482,7 @@ public class ZestGuidance implements Guidance {
     private void updateCoverageFile() {
         try {
             PrintWriter pw = new PrintWriter(coverageFile);
-            pw.write("Total: " + getTotalCoverage().hashCode() + "\n");
+            pw.write(getTotalCoverage().toString());
             pw.close();
         } catch (FileNotFoundException ignore) {
             throw new GuidanceException(ignore);
