ff52e79e9ed7697021d9db4d58feffa0decadddd
Rohan Padhye <rohanpadhye@cmu.edu>
2021-08-06 18:02:25 -0400

    GuidedFuzzing: Allow setting/unsetting guidance from outside

--- fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/junit/GuidedFuzzing.java
+++ fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/junit/GuidedFuzzing.java
@@ -62,9 +62,9 @@ public class GuidedFuzzing {
      * @param g the guidance instance
      * @throws IllegalStateException if a guidance has already been set
      */
-    private static void setGuidance(Guidance g) {
+    public static synchronized void setGuidance(Guidance g) {
         if (guidance != null) {
-            throw new IllegalStateException("Can only set guided once.");
+            throw new IllegalStateException("Cannot set more than one guidance simultaneously");
         }
         guidance = g;
     }
@@ -89,7 +89,7 @@ public class GuidedFuzzing {
      * This property is ensured by {@link GuidedFuzzing#run(Class, String, Guidance, PrintStream)}.
      *
      */
-    private static void unsetGuidance() {
+    public static synchronized void unsetGuidance() {
         guidance = null;
         TraceLogger.get().remove();
     }
