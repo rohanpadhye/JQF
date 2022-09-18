c40e19611cb8951ae83b5e1b099f2de1f8d468ef
Ao Li <leeleo3x@gmail.com>
2022-09-18 09:17:12 -0400

    Fix screen command.

--- scripts/experiments/ei/bench.sh
+++ scripts/experiments/ei/bench.sh
@@ -40,8 +40,10 @@ export JVM_OPTS="$JVM_OPTS -XX:-UseGCOverheadLimit -Xmx16g"
 
 SNAME="$NAME-$e"
 
+
+
 FAST_ENV="\"$JVM_OPTS -DuseFastNonCollidingCoverageInstrumentation=true\""
-screen -S "$SNAME" -X screen -t ei_fast_$e
+screen -S "$SNAME" -dm -t ei_fast_$e
 screen -S "$SNAME" -X screen -t zest_fast_$e
 screen -S "$SNAME" -p ei_fast_$e -X stuff "JVM_OPTS=$FAST_ENV timeout $TIME $JQF_EI -c \$($JQF_DIR/scripts/examples_classpath.sh) -D-DuseFastNonCollidingCoverageInstrumentation=true $TEST_CLASS testWithGenerator $EI_FAST_OUT_DIR^M"
 screen -S "$SNAME" -p zest_fast_$e -X stuff "JVM_OPTS=$FAST_ENV timeout $TIME $JQF_ZEST -c \$($JQF_DIR/scripts/examples_classpath.sh) $TEST_CLASS testWithGenerator $ZEST_FAST_OUT_DIR^M"
