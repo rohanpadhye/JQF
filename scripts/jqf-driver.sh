961e710e71c6f60e4fc54b5879a97bbc794293d0
Rohan Padhye <rohanpadhye@cmu.edu>
2023-03-02 22:07:49 -0500

    CLI script: Bump ASM version
    Fixes #212

--- scripts/jqf-driver.sh
+++ scripts/jqf-driver.sh
@@ -19,7 +19,7 @@ INST_JAR="${INST_DIR}/$project-instrument-$version.jar"
 
 # Compute classpaths (the /classes are only for development; 
 #   if empty the JARs will have whatever is needed)
-INST_CLASSPATH="${INST_DIR}/classes:${INST_JAR}:${INST_DIR}/dependency/asm-9.1.jar"
+INST_CLASSPATH="${INST_DIR}/classes:${INST_JAR}:${INST_DIR}/dependency/asm-9.4.jar"
 FUZZ_CLASSPATH="${FUZZ_DIR}/classes:${FUZZ_JAR}"
 
 # If user-defined classpath is not set, default to '.'
