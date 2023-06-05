c40d0e97073308be76b6ebe846358c9985ff1acd
Rohan Padhye <rohanpadhye@cmu.edu>
2023-06-05 12:24:59 -0400

    Update classpath.sh to include instrument package
    Co-authored-by: Yuqi Huai (@YuqiHuai)
    
    Fixes broken tutorial instructions (#233)

--- scripts/classpath.sh
+++ scripts/classpath.sh
@@ -8,10 +8,17 @@ popd > /dev/null
 # The root dir is one up
 ROOT_DIR=`dirname $SCRIPT_DIR`
 
-# Create classpath
+# Create classpath using all classes from and dependencies of the `fuzz` and `instrument` packages
+
 cp="$ROOT_DIR/fuzz/target/classes:$ROOT_DIR/fuzz/target/test-classes"
 
 for jar in $ROOT_DIR/fuzz/target/dependency/*.jar; do
+  cp="$cp:$jar"
+done
+
+cp="$cp:$ROOT_DIR/instrument/target/classes:$ROOT_DIR/instrument/target/test-classes"
+
+for jar in $ROOT_DIR/instrument/target/dependency/*.jar; do
   cp="$cp:$jar"
 done
 
