88f26d45636c0adc119c507645e265f3836df643
Magnus Reftel <Magnus.Reftel@skatteetaten.no>
2024-06-20 11:16:27 +0200

    test IOUtils

---
+++ fuzz/src/test/java/edu/berkeley/cs/jqf/fuzz/util/IOUtilsTest.java
@@ -0,0 +1,36 @@
+package edu.berkeley.cs.jqf.fuzz.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class IOUtilsTest {
+    @Rule
+    public TemporaryFolder temp = new TemporaryFolder();
+
+    @Test
+    public void shouldResolveSingleFile() throws IOException {
+        final File file = temp.newFile();
+        final File[] resolved = IOUtils.resolveInputFileOrDirectory(file);
+        Assert.assertEquals(1, resolved.length);
+        Assert.assertEquals(file, resolved[0]);
+    }
+
+    @Test
+    public void shouldResolveFilesInDirectory() throws IOException {
+        final File dir = temp.newFolder();
+        final File a = dir.toPath().resolve("a").toFile();
+        Assume.assumeTrue(a.createNewFile());
+        final File b = dir.toPath().resolve("b").toFile();
+        Assume.assumeTrue(b.createNewFile());
+        final File[] resolved = IOUtils.resolveInputFileOrDirectory(dir);
+        Assert.assertEquals(2, resolved.length);
+        Assert.assertEquals(a, resolved[0]);
+        Assert.assertEquals(b, resolved[1]);
+    }
+}
